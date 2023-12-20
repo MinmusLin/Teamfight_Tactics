123a5655471db910aa0fb38d9b91b779b9d98e77
MinmusLin <minmuslin@outlook.com>
2023-12-20 12:13:23 +0800

    Update Constant.h

--- proj.win32/Constant.h
+++ proj.win32/Constant.h
@@ -14,16 +14,16 @@
 #include <string>
 
 // 应用程序设置
-constexpr int DESIGN_RESOLUTION_WIDTH = 1280;      // 设计分辨率宽度
-constexpr int DESIGN_RESOLUTION_HEIGHT = 720;      // 设计分辨率高度
-constexpr int SMALL_RESOLUTION_WIDTH = 960;        // 小分辨率宽度
-constexpr int SMALL_RESOLUTION_HEIGHT = 540;       // 小分辨率高度
-constexpr int MEDIUM_RESOLUTION_WIDTH = 1280;      // 中分辨率宽度
-constexpr int MEDIUM_RESOLUTION_HEIGHT = 720;      // 中分辨率高度
-constexpr int LARGE_RESOLUTION_WIDTH = 1920;       // 大分辨率宽度
-constexpr int LARGE_RESOLUTION_HEIGHT = 1080;      // 大分辨率高度
-constexpr float FRAME_RATE = 60.0f;                // 应用程序帧率
-const std::string APPLICATION_TITLE = "金铲铲之战"; // 应用程序标题
+constexpr int DESIGN_RESOLUTION_WIDTH = 1280;                        // 设计分辨率宽度
+constexpr int DESIGN_RESOLUTION_HEIGHT = 720;                        // 设计分辨率高度
+constexpr int SMALL_RESOLUTION_WIDTH = 960;                          // 小分辨率宽度
+constexpr int SMALL_RESOLUTION_HEIGHT = 540;                         // 小分辨率高度
+constexpr int MEDIUM_RESOLUTION_WIDTH = 1280;                        // 中分辨率宽度
+constexpr int MEDIUM_RESOLUTION_HEIGHT = 720;                        // 中分辨率高度
+constexpr int LARGE_RESOLUTION_WIDTH = 1920;                         // 大分辨率宽度
+constexpr int LARGE_RESOLUTION_HEIGHT = 1080;                        // 大分辨率高度
+constexpr float FRAME_RATE = 60.0f;                                  // 应用程序帧率
+const std::string APPLICATION_TITLE = "金铲铲之战 Teamfight Tactics"; // 应用程序标题
 
 // 网络环境设置
 constexpr int HOSTNAME_LENGHT = 128;                               // 主机名长度
