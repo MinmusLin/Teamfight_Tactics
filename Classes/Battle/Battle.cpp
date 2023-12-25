6b446844376c4053dbff29779ccd74696cdb37a2
YangRuakaka <141391836+YangRuakaka@users.noreply.github.com>
2023-12-25 14:15:33 +0800

    battle commit

--- Classes/Battle/Battle.cpp
+++ Classes/Battle/Battle.cpp
@@ -8,14 +8,19 @@
 
 #include <iostream>
 #include "Battle.h"
+#include "Champion/Champion.h"
 
+<<<<<<< Updated upstream
 // 构造函数
+=======
+ // 构造函数
+>>>>>>> Stashed changes
 Battle::Battle(const ChampionCategory myFlagMap[][BATTLE_MAP_COLUMNS], const ChampionCategory enemyFlagMap[][BATTLE_MAP_COLUMNS])
 {
     for (int i = 0; i < PLACE_MAP_ROWS; i++) {
         for (int j = 0; j < BATTLE_MAP_COLUMNS; j++) {
             championCategoryMap[i][j] = myFlagMap[i][j];
-            championCategoryMap[PLACE_MAP_ROWS - 1 - i][BATTLE_MAP_COLUMNS - 1 - j] = enemyFlagMap[i][j];
+            championCategoryMap[BATTLE_MAP_ROWS - 1 - i][BATTLE_MAP_COLUMNS - 1 - j] = enemyFlagMap[i][j];
         }
     }
     for (int i = 0; i < BATTLE_MAP_ROWS; i++) {
@@ -28,12 +33,28 @@ Battle::Battle(const ChampionCategory myFlagMap[][BATTLE_MAP_COLUMNS], const Cha
                     std::cerr << "Memory allocation failed: " << e.what() << std::endl;
                     throw;
                 }
+                championMap[i][j]->setCurrentPosition(i, j);
+                // 设置阵营
+                if (i < 4) {
+                    myCount++;
+                    championMap[i][j]->setCamp(true);
+                }
+                else {
+                    enemyCount++;
+                    championMap[i][j]->setCamp(false);
+                }
             }
             else {
                 championMap[i][j] = nullptr;
             }
         }
     }
+}
+
+// 获取场上英雄信息
+Champion* Battle::getChampion(const int& x, const int& y)
+{
+    return championMap[x][y];
 }
 
 // 析构函数
@@ -46,4 +67,65 @@ Battle::~Battle()
             }
         }
     }
+}
+
+// 获取场上我方剩余数量
+int Battle::getMyCount()
+{
+    return myCount;
+}
+
+// 获取场上敌方剩余数量
+int Battle::getEnemyCount()
+{
+    return enemyCount;
+}
+
+// 增加敌方数量
+void Battle::addEnemyCount()
+{
+    enemyCount++;
+}
+
+// 增加我方数量
+void Battle::addMyCount()
+{
+    myCount++;
+}
+
+// 重置数量
+void Battle::resetCount()
+{
+    enemyCount = 0;
+    myCount = 0;
+}
+
+// 将一个位置置空
+void Battle::setEmpty(const int& x, const int& y)
+{
+    championMap[x][y] = nullptr;
+}
+
+// 放置一个棋子
+void Battle::placeChampion(const int& x, const int& y, Champion* champion)
+{
+    championMap[x][y] = champion;
+}
+
+// 胜利
+void Battle::setWin()
+{
+    isWinning = win;
+}
+
+// 失败
+void Battle::setLose()
+{
+    isWinning = lose;
+}
+
+// 平局
+void Battle::setDraw()
+{
+    isWinning = draw;
 }
--- Classes/Battle/Battle.h
+++ Classes/Battle/Battle.h
@@ -12,15 +12,55 @@
 
 #include "Champion/Champion.h"
 #include "proj.win32/Constant.h"
+#include "Champion/Champion.h"
+
+class Champion;
 
 /*
  * Class Name:     Battle
  * Class Function: 对战类
  */
 class Battle {
+    friend class Champion;
 public:
     // 构造函数
     Battle(const ChampionCategory myFlagMap[][BATTLE_MAP_COLUMNS], const ChampionCategory enemyFlagMap[][BATTLE_MAP_COLUMNS]);
+<<<<<<< Updated upstream
+=======
+
+    // 获取场上英雄信息
+    Champion* getChampion(const int& x, const int& y);
+
+    // 获取场上我方剩余数量
+    int getMyCount();
+
+    // 获取场上敌方剩余数量
+    int getEnemyCount();
+
+    // 增加敌方数量
+    void addEnemyCount();
+
+    // 增加我方数量
+    void addMyCount();
+
+    // 重置数量
+    void resetCount();
+
+    // 将一个位置置空
+    void setEmpty(const int& x, const int& y);
+
+    // 放置一个棋子
+    void placeChampion(const int& x, const int& y,Champion* champion);
+
+    // 胜利
+    void setWin();
+
+    // 失败
+    void setLose();
+
+    // 平局
+    void setDraw();
+>>>>>>> Stashed changes
 
     // 析构函数
     ~Battle();
@@ -28,6 +68,9 @@ public:
 private:
     ChampionCategory championCategoryMap[BATTLE_MAP_ROWS][BATTLE_MAP_COLUMNS]; // 战斗英雄地图
     Champion* championMap[BATTLE_MAP_ROWS][BATTLE_MAP_COLUMNS];                // 战斗英雄指针
+    int myCount ;                                                              // 我方英雄数量
+    int enemyCount ;                                                           // 敌方英雄数量
+    int isWinning;                                                            // 是否胜利
 };
 
 #endif // !_BATTLE_H_
